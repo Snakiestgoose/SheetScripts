62f97585e4e4c3c87f70bc63ee10bed687d4d9c5
ahobgood <hobogod3@gmail.com>
2022-07-24 12:46:30 -0500

    chore: updated script to update each row missing info

--- lego.py
+++ lego.py
@@ -22,82 +22,97 @@ import json
 import pygsheets
 import pandas as pd
 import logging
-
 # Import env variables
 from dotenv import load_dotenv
 load_dotenv("config.env")
 import os
-file = os.getenv("GDRIVE_API_CREDENTIALS")
-lego_key = os.getenv("REBRICKABLE_API_KEY")
 
-logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.DEBUG)
-logging.debug('File: %s', file)
+def main():
+    file = os.getenv("GDRIVE_API_CREDENTIALS")
+    lego_key = os.getenv("REBRICKABLE_API_KEY")
 
-#authorization
-gc = pygsheets.authorize(service_file=file)
+    logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.DEBUG)
+    logging.debug('File: %s', file)
 
-# Create empty dataframe
-df = pd.DataFrame()
-# Create a column
-df['lego'] = ['1', '2', '3']
+    #authorization
+    gc = pygsheets.authorize(service_file=file)
+    #open the google spreadsheet (where 'PY to Gsheet Test' is the name of my sheet)
+    sh = gc.open('lego_sheet')
+    #select the first sheet 
+    wks = sh.worksheet_by_title("HaveList")
 
+    # TODO: Set range with wks.rows
+    cell_range = wks.range('A4:B10', returnas='matrix')
 
-#open the google spreadsheet (where 'PY to Gsheet Test' is the name of my sheet)
-sh = gc.open('lego_sheet')
+    logging.debug('\n')
+    for id in range(wks.rows):
+        logging.debug('id: %s', cell_range[id])
+        if not cell_range[id][0]:
+            break
+        else:
+            # Add id to list
+            if cell_range[id][1] == "":
+                legoId = cell_range[id][0]
+                rebrickableData(legoId, lego_key, wks, id)
+                logging.debug('ids" %s', legoId)
 
-#select the first sheet 
-wks = sh.worksheet_by_title("HaveList")
+    logging.debug('\n')
 
-#update the first sheet with df, starting at cell B2. 
-wks.set_dataframe(df,(8,8))
+# Rebrickable API queries
+def rebrickableData(legoId, legoKey, wks, rowIndex):
+    # range starts at A4, id starts at 0 in loop above 
+    rowIndex += 4
 
-# TODO: Set range with wks.rows
-cell_range = wks.range('A4:B10', returnas='matrix')
+    import requests
+    from datetime import datetime
+    from dateutil import parser
 
-legoIdSet = set()
-logging.debug('\n')
-for id in range(wks.rows):
-    logging.debug('id: %s', cell_range[id])
-    if not cell_range[id][0]:
-        break
-    else:
-        # Add id to list
-        if cell_range[id][1] == "":
-            legoIdSet.add(cell_range[id][0])
-
-logging.debug('ids" %s', legoIdSet)
-logging.debug('\n')
-
-# TODO: 
-#   determine rows that aren't populated
-#   query for entry information at Rebrickable API
-#       EX: https://rebrickable.com/api/v3/lego/sets/75306/
-#   sort by valid entry
-
-import requests
-
-for id in legoIdSet:
-    lego_id = "{}{}".format(id, "-1")
-    api_url = "https://rebrickable.com/api/v3/lego/sets/{}/?key={}".format(lego_id, lego_key)
+    lego_id = "{}{}".format(legoId, "-1")
+    api_url = "https://rebrickable.com/api/v3/lego/sets/{}/?key={}".format(lego_id, legoKey)
     response = requests.get(api_url)
     jsonresponse = response.json()
     
     newSet = LegoSet()
     if response.status_code == 200:
         newSet.name = jsonresponse["name"]
+        newSet.year = jsonresponse["year"]
+        newSet.theme_id = jsonresponse["theme_id"]
+        newSet.num_parts = jsonresponse["num_parts"]
+        newSet.set_img_url = jsonresponse["set_img_url"]
+        newSet.set_url = jsonresponse["set_url"]
+        newSet.last_modified_dt = jsonresponse["last_modified_dt"]
     else:
         newSet.name = "Invalid set ID"
 
     logging.debug('response: %s', jsonresponse)
     logging.debug('newSet: %s', newSet.name)
     logging.debug('\n')
+
+    
+
     #TODO: Update row with LegoSet info
+    for i in range(2,9):
+        logging.debug('rowIndex: %s', rowIndex)
+        logging.debug('i: %s', i)
+        c1 = wks.cell((rowIndex, i))
 
-    c1 = wks.cell('B4')
-    c1.value = newSet.name
+        if i == 2:
+            c1.value = newSet.name
+        elif i == 3:
+            c1.value = newSet.year
+        elif i == 4:
+            c1.value = newSet.theme_id
+        elif i == 5:
+            c1.value = newSet.num_parts
+        elif i == 6:
+            c1.value = newSet.set_img_url
+        elif i == 7:
+            c1.value = newSet.set_url
+        elif i == 8:
+            c1.value = newSet.last_modified_dt
+        
 
+main()
 
-
-#lego_id = "75306-1"
 
 
